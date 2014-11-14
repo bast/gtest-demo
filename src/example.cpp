a31f6b145f4d70378d45bdc76ffebae2aea1d7e0
Radovan Bast <rbast@users.noreply.github.com>
2014-11-14 13:25:26 +0100

    initial layout

---
+++ src/example.cpp
@@ -0,0 +1,17 @@
+
+#include "example.h"
+
+double add_numbers(const double f1, const double f2)
+{
+    return f1 + f2;
+}
+
+double subtract_numbers(const double f1, const double f2)
+{
+    return f1 - f2;
+}
+
+double multiply_numbers(const double f1, const double f2)
+{
+    return f1*f2;
+}
---
+++ src/example.h
@@ -0,0 +1,10 @@
+#ifndef example_h_
+#define example_h_
+
+double add_numbers(const double f1, const double f2);
+
+double subtract_numbers(const double f1, const double f2);
+
+double multiply_numbers(const double f1, const double f2);
+
+#endif
