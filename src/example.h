88f6dcdaae52d9aeb02cd408a65e97e8d8c41d8d
Radovan Bast <bast@users.noreply.github.com>
2018-03-13 14:44:33 +0100

    use pragma once

--- src/example.h
+++ src/example.h
@@ -1,10 +1,7 @@
-#ifndef EXAMPLE_H_INCLUDED
-#define EXAMPLE_H_INCLUDED
+#pragma once
 
 double add_numbers(const double f1, const double f2);
 
 double subtract_numbers(const double f1, const double f2);
 
 double multiply_numbers(const double f1, const double f2);
-
-#endif /* EXAMPLE_H_INCLUDED */
