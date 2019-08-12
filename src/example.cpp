d775a84c4f48243c105e51b9b8482b4ff1f52670
Pahandrovich <35104773+Pahandrovich@users.noreply.github.com>
2019-08-12 15:01:30 +0300

    Update by Pahatkovich

--- src/example.cpp
+++ src/example.cpp
@@ -3,11 +3,13 @@
 double add_numbers(const double f1, const double f2)
 {
     return f1 + f2;
+    printf("Du hast");
 }
 
 double subtract_numbers(const double f1, const double f2)
 {
     return f1 - f2;
+    printf("Vyacheslavovich");
 }
 
 double multiply_numbers(const double f1, const double f2)
