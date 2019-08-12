3bd88cbc22c8768c6a589d6739e65ccbde60cbdf
GalyaZalesskaya <33455576+GalyaZalesskaya@users.noreply.github.com>
2019-08-12 15:22:29 +0300

    Update example.cpp

--- src/example.cpp
+++ src/example.cpp
@@ -10,7 +10,7 @@ double add_numbers(const double f1, const double f2)
 
 double subtract_numbers(const double f1, const double f2)
 {
-    std::cout>>"I'm substracting numbers"
+    std::cout << "I'm substracting numbers" << std::endl;
     return f1 - f2;
     std::cout <<"Vyacheslavovich";
 }
