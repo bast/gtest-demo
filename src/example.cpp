d5f6196db078c2c51143e212c74300d648ec3075
Roberto Di Remigio <roberto.diremigio@gmail.com>
2021-02-22 11:32:21 +0100

    Format sources with clang-format

--- src/example.cpp
+++ src/example.cpp
@@ -1,16 +1,7 @@
 #include "example.h"
 
-double add_numbers(const double f1, const double f2)
-{
-    return f1 + f2;
-}
+double add_numbers(const double f1, const double f2) { return f1 + f2; }
 
-double subtract_numbers(const double f1, const double f2)
-{
-    return f1 - f2;
-}
+double subtract_numbers(const double f1, const double f2) { return f1 - f2; }
 
-double multiply_numbers(const double f1, const double f2)
-{
-    return f1 * f2;
-}
+double multiply_numbers(const double f1, const double f2) { return f1 * f2; }
--- test/example_subtract.cpp
+++ test/example_subtract.cpp
@@ -2,9 +2,8 @@
 
 #include "example.h"
 
-TEST(example, subtract)
-{
-    double res;
-    res = subtract_numbers(1.0, 2.0);
-    ASSERT_NEAR(res, -1.0, 1.0e-11);
+TEST(example, subtract) {
+  double res;
+  res = subtract_numbers(1.0, 2.0);
+  ASSERT_NEAR(res, -1.0, 1.0e-11);
 }
